dc4e4ecea0483b270d095115b45f22ce5dcf5fe6
ahhatemp <h4a1k4l@gmail.com>
2025-09-11 21:55:55 +0700

    Hapus jenis layanan

--- Praktikum 2 Laundry.cpp
+++ Praktikum 2 Laundry.cpp
@@ -16,7 +16,7 @@ string formatRupiah(int angka)
 
 int main()
 {
-    string layanan, jenis;
+    string layanan;
     float berat, harga, total;
 
     cout << "=== Praktikum 2(Laundry kiloan) ===" << endl;
@@ -24,18 +24,12 @@ int main()
     cout << "1. Cuci saja - Rp3.000/kg" << endl;
     cout << "2. Setrika saja - Rp5.000/kg" << endl;
     cout << "3. Cuci setrika - Rp6.000/kg" << endl;
-    cout << "Jenis layanan: (Reguler/Express) Express: +50% dari total harga" << endl;
 
     cout << "Masukkan jenis layanan (1/2/3): ";
     cin >> layanan;
 
     cout << "Masukkan berat pakaian (kg): ";
     cin >> berat;
-    cin.ignore();
-
-    cout << "Jenis layanan(Reguler/Express): " ;
-    getline(cin, jenis);
-    transform(jenis.begin(), jenis.end(), jenis.begin(), ::tolower);
 
     if (layanan == "1") 
     {
@@ -55,16 +49,10 @@ int main()
 
     total = harga * berat;
 
-    if (jenis == "express") 
-    {
-        total = total + (0.5 * total);
-    }
-
     cout << endl;
     cout << "\n== Struk Pembayaran ==:" << endl;
     cout << "Layanan: " << layanan << endl;
     cout << "Berat pakaian: " << berat << " kg" << endl;
-    cout << "Jenis layanan: " << jenis << endl;
     cout << "Total harga: " << formatRupiah(total) << endl;
 
     return 0;
