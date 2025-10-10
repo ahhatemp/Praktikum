1d89de7b1c9be4969fe09da173115cd75ceddeba
ahhatemp <h4a1k4l@gmail.com>
2025-10-10 13:27:01 +0700

    Praktikum 5 switch case

--- Tugas Praktikum 4.cpp
+++ Tugas Praktikum 4.cpp
@@ -5,30 +5,47 @@ using namespace std;
 int main() {
     float berat;
     int max_berat = 20;
-    string layanan;
+    int layanan;
     int harga;
     int total;
 
-    cout << "Masukkan berat pakaian (kg): ";
-    cin >> berat;
-    if (berat <= 10) {
-        layanan = "Reguler";
-    } else if (berat <= 20) {
-        layanan = "Express";
-    } else {
-        layanan = "Kapasitas mesin penuh, pisahkan cucian!";
+    cout << "Pilih jenis layanan cucian:" << endl;
+    cout << "1. Cuci Reguler (Rp3000/kg)" << endl;
+    cout << "2. Cuci kilat (Rp5000/kg)" << endl;
+    cout << "3. Setrika (Rp4000/kg)" << endl;
+    cin >> layanan;
+
+    switch (layanan) {
+        case 1:
+            cout << "Anda memilih layanan Cuci Reguler" << endl;
+            harga = 3000;
+            cout << "Masukkan berat pakaian (kg): ";
+            cin >> berat;
+            break;
+        case 2:
+            cout << "Anda memilih layanan Cuci kilat" << endl;
+            harga = 5000;
+            cout << "Masukkan berat pakaian (kg): ";
+            cin >> berat;
+            break;
+        case 3:
+            cout << "Anda memilih layanan Setrika" << endl;
+            harga = 4000;
+            cout << "Masukkan berat pakaian (kg): ";
+            cin >> berat;
+            break;
+        default:
+            cout << "Layanan tidak tersedia" << endl;
+            return 0;
     }
 
-    if (layanan == "Reguler") {
-        harga = 3000;
-    } else if (layanan == "Express") {
-        harga = 5000;
-    } else {
-        harga = 0;
+    if (berat > max_berat) {
+        cout << "Kapasitas mesin penuh, pisahkan cucian!" << endl;
+        return 0;
     }
     total = harga * berat;
-    cout << "Jenis layanan: " << layanan << endl;
     cout << "Total harga: Rp" << total << endl;
+
 
     return 0;
 }
